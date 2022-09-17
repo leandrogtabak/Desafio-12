c0d5eae39c12981e7ce05c4c5b99688a88957159
Leandro Tabak <leandrogtabak@gmail.com>
2022-09-17 20:03:21 +0300

    Deleting db config files

--- DB/config.js
+++
@@ -1,8 +0,0 @@
-/*Configuracion Firebase*/
-
-export const urlJson = './DB/ecommerce-bd7e1-firebase-adminsdk-lckkb-420c11014d.json';
-export const urlDb = 'https://ecommerce-bd7e1.firebaseio.com';
-
-/*Configuracion Mongo */
-
-export const urlMongo = 'mongodb+srv://newuser:tfDLXnPCGy4RCE97@cluster0.wc4ea7z.mongodb.net/ecommerce?retryWrites=true&w=majority';
